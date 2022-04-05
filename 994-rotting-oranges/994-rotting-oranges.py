5a2656afb16ed3ec8adddac40aeec876b4eedcd8
benj35 <se.biniyam.abiy@gmail.com>
2022-04-05 22:26:19 +0300

    Time: 56 ms (83.92%), Space: 13.9 MB (50.84%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -3,10 +3,14 @@ class Solution:
         DIR = (-1,0,1,0,-1)
         inBound = lambda r, c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
         queue = deque()
+        fresh = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     queue.append((i, j))
+                elif grid[i][j] == 1:
+                    fresh.add((i,j))
+                    
         minute = 0
         while queue:
             length = len(queue)
@@ -16,14 +20,11 @@ class Solution:
                     newRow, newCol = row + DIR[i], col + DIR[i+1]
                     if inBound(newRow, newCol) and grid[newRow][newCol] == 1:
                         grid[newRow][newCol] = 2
+                        fresh.remove((newRow, newCol))
                         queue.append((newRow, newCol))
             minute+=1
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    return -1
-        
+            
+        if fresh: return -1
         return 0 if minute == 0 else minute-1
         
         
