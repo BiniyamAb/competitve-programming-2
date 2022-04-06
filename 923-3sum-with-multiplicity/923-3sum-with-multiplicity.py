75994ab2dc52519602d86e90d551d4e5d5ff1311
benj35 <se.biniyam.abiy@gmail.com>
2022-04-06 23:49:54 +0300

    Time: 111 ms (69.19%), Space: 14 MB (92.44%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -0,0 +1,14 @@
+
+
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        count = collections.Counter(arr)
+        ans = 0
+        for i, j in itertools.combinations_with_replacement(count, 2):
+            k = target - i - j
+            if i == j == k: ans += count[i] * (count[i] - 1) * (count[i] - 2) / 6
+            elif i == j != k: ans += count[i] * (count[i] - 1) / 2 * count[k]
+            elif k > i and k > j: ans += count[i] * count[j] * count[k]
+                
+        return int(ans % (10**9 + 7))
+        
