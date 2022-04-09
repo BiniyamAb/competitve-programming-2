02eb470a0f587400fff68c11ffa1eb65577d2866
benj35 <se.biniyam.abiy@gmail.com>
2022-04-09 21:36:42 +0300

    Time: 53 ms (68.10%), Space: 13.9 MB (80.54%) - LeetHub

--- 969-pancake-sorting/969-pancake-sorting.py
+++ 969-pancake-sorting/969-pancake-sorting.py
@@ -1,17 +1,17 @@
-class Solution:
-    def reverse(self, l, r, arr):
-        while l < r:
-            arr[l], arr[r] = arr[r], arr[l]
-            l, r = l+1, r-1
-        
+class Solution:        
     def pancakeSort(self, arr: List[int]) -> List[int]:
+        def reverse(l, r):
+            while l < r:
+                arr[l], arr[r] = arr[r], arr[l]
+                l, r = l+1, r-1
+                
         k_arr =[]
         for num in range(len(arr), 0, -1):
             ind = arr.index(num)
             if ind != 0:
-                self.reverse(0, ind, arr)
+                reverse(0, ind)
                 k_arr.append(ind+1)
-            self.reverse(0, num-1, arr)
+            reverse(0, num-1)
             k_arr.append(num)
             
         return k_arr
