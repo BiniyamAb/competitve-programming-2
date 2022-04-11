0e76955d0dccd5a9949d0b3efe7cd76055525d10
benj35 <se.biniyam.abiy@gmail.com>
2022-04-11 22:11:00 +0300

    Time: 371 ms (23.95%), Space: 14.4 MB (13.09%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,19 +1,8 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        m, n = len(grid), len(grid[0])
-        while k > 0:
-            _next = grid[0][0]
-            for i in range(m):
-                for j in range(n):
-                    if i == m - 1 and j == n - 1:
-                        grid[0][0], _next = _next, grid[0][0] 
-                    elif j == n - 1:
-                        grid[i+1][0], _next = _next, grid[i+1][0]
-                    else:
-                        grid[i][j+1], _next = _next, grid[i][j+1]
-            k-=1
-        
-        return grid
+        l, m, n, k = [num for row in grid for num in row], len(grid), len(grid[0]), k % (len(grid) * len(grid[0])) 
+        l = l[-k:] + l[:-k] 
+        return [l[i * n: i * n + n] for i in range(m)]
         
         
         
