54078cbf421d854466f024d98eda5a991d8b8faf
benj35 <se.biniyam.abiy@gmail.com>
2022-04-12 12:15:04 +0300

    Time: 47 ms (51.21%), Space: 14.5 MB (12.69%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -4,13 +4,11 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        visited, grid = set(), []
+        visited = set()
         DIR = (1,0,-1,0,1,1,-1,-1,1)
         inBound = lambda r, c: 0 <= r < len(board) and 0 <= c < len(board[0])
         for i in range(len(board)):
-            lst = []
-            for j in range(len(board[0])):lst.append(0)
-            grid.append(lst)
+            for j in range(len(board[0])): board[i][j] = [board[i][j], 0]
         
         def dfs(row, col, board):
             visited.add((row,col))
@@ -20,16 +18,16 @@ class Solution:
                 if inBound(newRow, newCol) and (newRow,newCol) not in visited:
                     live += dfs(newRow, newCol, board)
                 elif inBound(newRow, newCol):
-                    live += board[newRow][newCol]
+                    live += board[newRow][newCol][0]
                     
-            if board[row][col] == 1 and (live > 3 or live < 2): grid[row][col] = 0
-            elif board[row][col] == 0 and live == 3: grid[row][col] = 1
-            else: grid[row][col] = board[row][col]
-            return board[row][col]
+            if board[row][col][0] == 1 and (live > 3 or live < 2): board[row][col][1] = 0
+            elif board[row][col][0] == 0 and live == 3: board[row][col][1] = 1
+            else: board[row][col][1] = board[row][col][0]
+            return board[row][col][0]
         
         dfs(0, 0, board)
-        for i in range(len(grid)): 
-            for j in range(len(grid[0])): board[i][j] = grid[i][j]
+        for i in range(len(board)): 
+            for j in range(len(board[0])): board[i][j] = board[i][j][1]
         
         
                 
