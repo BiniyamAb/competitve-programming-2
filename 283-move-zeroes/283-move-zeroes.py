329d7d3756df9d56e01f0dfa0cc65bc881ec911c
benj35 <se.biniyam.abiy@gmail.com>
2022-04-15 16:09:14 +0300

    Time: 160 ms (98.40%), Space: 15.6 MB (20.30%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pointer = zeros = n = 0
+        p = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[pointer] = nums[i]
-                pointer+=1
-            else: zeros+=1
-        
-        for j in range(len(nums)-1, len(nums) - zeros - 1, -1): nums[j] = 0
+                nums[p] = nums[i]
+                p+=1
+        for j in range(p, len(nums)): nums[j] = 0
         
