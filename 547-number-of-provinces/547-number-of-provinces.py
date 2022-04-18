694b6f399cb78f1fe0f83d92a56cb4bf9fbfdcfd
benj35 <se.biniyam.abiy@gmail.com>
2022-04-18 18:49:51 +0300

    Time: 266 ms (53.45%), Space: 14.3 MB (53.98%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,17 +1,28 @@
 class Solution:
-    def dfs(self, row, isConnected):
-        self.visited.add(row)
-        for col in range(len(isConnected[row])):
-            if row != col and isConnected[row][col] == 1 and col not in self.visited:
-                self.dfs(col, isConnected)
-    
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        provinces = 0
-        self.visited = set()
-        for row in range(len(isConnected)):
-            if row not in self.visited:
-                self.dfs(row, isConnected)
-                provinces+=1
+        n = len(isConnected)
+        root = [i for i in range(n)]
+        rank = [1] * n
+        def find(x):
+            if x == root[x]:
+                return x
+            root[x] = find(root[x])
+            return root[x]
         
-        return provinces
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    root[rootY] = rootX
+                elif rank[rootY] > rank[rootX]:
+                    root[rootX] = rootY
+                else:
+                    root[rootX] = rootY
+                    rank[rootY] += 1
         
+        for x in range(n):
+            for y in range(len(isConnected[0])):
+                if x!=y and (isConnected[x][y] == 1 or find(x) == find(y)): union(x, y)
+        
+        return len(set(root))
