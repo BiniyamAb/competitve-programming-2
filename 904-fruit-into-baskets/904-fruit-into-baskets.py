ee4aa152afb8d9d02f3b27e424a81bff7419cfc3
benj35 <se.biniyam.abiy@gmail.com>
2022-04-21 23:07:53 +0300

    Time: 1321 ms (22.79%), Space: 20.2 MB (39.66%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -13,13 +13,12 @@ class Solution:
             elif len(types) < 2:
                 types.append([fruits[r], r])
                 continue
-            print(r)
+
             max_pick = max(max_pick, r-l)
             types.sort(key=lambda x: x[1], reverse=True)
             l = types.pop()[1] + 1
             types.append([fruits[r], r])
-        
-        print(len(fruits), l)
+ 
         return max(max_pick, len(fruits) - l)
             
             
