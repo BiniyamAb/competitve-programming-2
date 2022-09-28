1af1a3794005f934aac13469f266606b78141dac
benj35 <se.biniyam.abiy@gmail.com>
2022-09-28 15:05:20 +0300

    Time: 3538 ms (22.11%), Space: 25 MB (39.56%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -2,27 +2,23 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):   
         self.matrix = matrix
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                self.matrix[i][j] += self.calculatePrefix(i,j)
-        
-        print(self.matrix)
+        temp = [0] * (len(self.matrix[0]) + 1)
+        self.matrix.append(temp)
+
+        for i in range(len(matrix)-1):
+            self.matrix[i].append(0)
+            for j in range(len(matrix[i])-1):
+                self.matrix[i][j] += self.calculatePrefix(i,j,i,j)
     
-    def calculatePrefix(self,r,c):
-        upper = left = upperleft = 0
-        if r != 0: upper = self.matrix[r-1][c]
-        if c != 0: left = self.matrix[r][c-1]
-        if c != 0 and r != 0: upperleft = self.matrix[r-1][c-1]
+    def calculatePrefix(self,r,c,r2,c2):
+        upper = self.matrix[r-1][c2]
+        left = self.matrix[r2][c-1]
+        upperleft = self.matrix[r-1][c-1]
         
         return upper + left - upperleft
 
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:  
-        upper = left = upperleft = 0
-        if row1 != 0: upper = self.matrix[row1-1][col2]
-        if col1 != 0: left = self.matrix[row2][col1-1]
-        if col1 != 0 and row1 != 0: upperleft = self.matrix[row1-1][col1-1]
-            
-        return self.matrix[row2][col2] - (upper + left - upperleft)
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:              
+        return self.matrix[row2][col2] - self.calculatePrefix(row1,col1,row2,col2)
         
         
 
