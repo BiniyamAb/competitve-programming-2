646b14ecb124fdd87aaf4da41f8920836a35a297
benj35 <se.biniyam.abiy@gmail.com>
2022-10-13 17:18:25 +0300

    Time: 74 ms (97.00%), Space: 18.4 MB (77.91%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -12,38 +12,24 @@ class Solution:
             if root.val == key: break  
             if root.val > key: parent, root, right = root, root.left, False
             else: parent, root, right = root, root.right, True
-                
         if not root: return node
         if (not root.right and not root.left) and not parent and root.val == key: return
-        
-        
         if not root.right or not root.left:
             if root.right and not root.left: chosen = root.right
             elif root.left and not root.right: chosen = root.left
             elif not root.left and not root.right: chosen = None
-                
             if not parent: return chosen
             if right: parent.right = chosen
             else: parent.left = chosen
             return node
-        
         else:
             leftChild = root.left
             rightChild = root.right
             while rightChild.left: rightChild = rightChild.left
-        
             rightChild.left = leftChild
             if root == node: return root.right
             else:
                 chosen = root.right
                 if right: parent.right = chosen
                 else: parent.left = chosen
-                
                 return node
-                
-                
-                    
-            
-        
-        
-        
