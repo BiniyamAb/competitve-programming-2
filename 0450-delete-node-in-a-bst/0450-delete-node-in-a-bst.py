46f407603008b844f2fae89a134a2b142b5280fc
benj35 <se.biniyam.abiy@gmail.com>
2022-10-13 17:08:29 +0300

    Time: 216 ms (5.14%), Space: 18.5 MB (27.02%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -7,54 +7,37 @@
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root: return root
-        node = root
-        parent = None
-        right = False
+        node, parent, right = root, None, False
         while root:
-            if root.val == key:
-                break
-            if root.val > key:
-                parent = root
-                root = root.left
-                right = False
-            else:
-                parent = root
-                root = root.right
-                right = True
+            if root.val == key: break  
+            if root.val > key: parent, root, right = root, root.left, False
+            else: parent, root, right = root, root.right, True
                 
         if not root: return node
-        if (not root.right and not root.left) and not parent and root.val == key: 
-            
-            return None
+        if (not root.right and not root.left) and not parent and root.val == key: return
+        
+        
         if not root.right or not root.left:
-            if root.right and not root.left:
-                chosen = root.right
-            elif root.left and not root.right:
-                chosen = root.left
-            elif not root.left and not root.right:
-                chosen = None
+            if root.right and not root.left: chosen = root.right
+            elif root.left and not root.right: chosen = root.left
+            elif not root.left and not root.right: chosen = None
+                
             if not parent: return chosen
-            if right:
-                parent.right = chosen
-            else:
-                parent.left = chosen
+            if right: parent.right = chosen
+            else: parent.left = chosen
             return node
         
         else:
             leftChild = root.left
             rightChild = root.right
-            while rightChild.left:
-                rightChild = rightChild.left
-            
+            while rightChild.left: rightChild = rightChild.left
+        
             rightChild.left = leftChild
-            if root == node:
-                return root.right
+            if root == node: return root.right
             else:
                 chosen = root.right
-                if right:
-                    parent.right = chosen
-                else:
-                    parent.left = chosen
+                if right: parent.right = chosen
+                else: parent.left = chosen
                 
                 return node
                 
