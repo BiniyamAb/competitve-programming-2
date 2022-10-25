37c2a9d9f57b86ea43d460edf540a5bef4ab9be3
benj35 <se.biniyam.abiy@gmail.com>
2022-10-25 15:16:07 +0300

    Time: 764 ms (54.72%), Space: 20.1 MB (5.51%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        LPS = [0] * len(s)
+        pointer = 0
+        for i in range(1,len(s)):
+            while pointer != 0 and s[pointer] != s[i]:
+                pointer = LPS[pointer-1]
+            if s[i] == s[pointer]:
+                LPS[i] = pointer + 1
+                pointer += 1
+        
+        res = []
+        for i in range(LPS[-1]):
+            res.append(s[i])
+        
+        return "".join(res)
+            
