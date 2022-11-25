85def3dae048c882e5249e67d6954da340596d4c
benj35 <se.biniyam.abiy@gmail.com>
2022-11-25 18:35:51 +0300

    Time: 1601 ms (87.64%), Space: 251.3 MB (5.99%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -4,16 +4,10 @@ class Solution:
         @cache
         def dp(l,r):
             if l > r: return 0
-            if l == r: return 1
-            
             if s[l] == s[r]:
-                return dp(l+1,r-1) + 2
-            else:
-                to_right = dp(l+1,r)
-                to_left = dp(l,r-1)
-                return max(to_right, to_left)
+                return dp(l+1,r-1) + 2 - int(l == r)
+            return max(dp(l+1,r), dp(l,r-1))
         
-        result = dp(0,len(s)-1)
-        return result
+        return dp(0,len(s)-1)
                 
         
