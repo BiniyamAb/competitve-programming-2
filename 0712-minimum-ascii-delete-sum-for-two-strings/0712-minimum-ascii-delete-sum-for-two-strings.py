ae1e4ae3021fd1f17e534be759d5f5d8175b603a
benj35 <se.biniyam.abiy@gmail.com>
2022-11-25 19:58:14 +0300

    Time: 1983 ms (44.93%), Space: 171.1 MB (18.56%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -5,14 +5,9 @@ class Solution:
         @cache
         def dp(i, j):
             if i >= N or j >= M: return 0
-            
             if s1[i] == s2[j]:
                 return (ord(s1[i]) * 2) + dp(i + 1, j + 1)
-            
             return max(dp(i + 1, j), dp(i, j + 1))
         
-        total = 0
-        for s in s1: total += ord(s)
-        for s in s2: total += ord(s)
-        
+        total = sum(ord(s) for s in s1) + sum(ord(s) for s in s2)
         return total - dp(0, 0)
