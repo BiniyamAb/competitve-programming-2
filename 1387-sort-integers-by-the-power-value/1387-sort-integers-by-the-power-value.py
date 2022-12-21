86671f58b1a3329483ade1f47ea5f5245e22ad77
benj35 <se.biniyam.abiy@gmail.com>
2022-12-21 08:11:14 +0300

    Time: 1400 ms (24.76%), Space: 14.3 MB (57.41%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @cache
+    def getPowerValue(self, x):
+        if x == 1: 
+            return 0
+        
+        if x % 2 == 0:
+            return self.getPowerValue(x // 2) + 1
+        
+        return self.getPowerValue((x * 3) + 1) + 1
+    
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        power_val = {}
+        nums = [i for i in range(lo,hi+1)]
+        for num in nums:
+            curr_power_val = self.getPowerValue(num)
+            power_val[num] = curr_power_val
+        
+        nums.sort(key=lambda num: (power_val[num], num))
+        return nums[k-1]
+        
