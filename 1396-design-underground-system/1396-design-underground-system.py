79bda973e5c533638b0c0a65cd563f347beedb97
benj35 <se.biniyam.abiy@gmail.com>
2023-01-03 11:39:36 +0300

    Time: 314 ms (65.46%), Space: 24.2 MB (55.75%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,24 +1,25 @@
 class UndergroundSystem:
 
     def __init__(self):
+        self.average_times = defaultdict(int)
+        self.num_of_travels = defaultdict(int)
         self.checkins = {}
-        self.times = {}
+        
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.checkins[id] = (stationName, t)
-
+        self.checkins[id] = (stationName,t)
+        
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        start_station, start_time = self.checkins.pop(id)
-        time = t - start_time
-        if self.times.get((start_station,stationName)) is not None:
-            self.times[(start_station,stationName)][0] += time
-            self.times[(start_station,stationName)][1] += 1
-        else:
-            self.times[(start_station,stationName)] = [time,1]
+        start_station, start_time = self.checkins[id]
+        path = (start_station,stationName)
+        average = (self.average_times[path] * self.num_of_travels[path]) + (t - start_time)
+        average /= (self.num_of_travels[path] + 1)
+        self.average_times[path] = average
+        self.num_of_travels[path] += 1
+        del self.checkins[id]
         
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        time, travels = self.times[(startStation,endStation)]
-        return time/travels
+        return self.average_times[(startStation,endStation)]
         
 
 
