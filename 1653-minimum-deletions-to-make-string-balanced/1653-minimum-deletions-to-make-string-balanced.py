90465e04a093fb0aa1d4f3029c6fd44da96f6eb2
benj35 <se.biniyam.abiy@gmail.com>
2023-02-14 22:19:34 +0300

    Time: 1552 ms (11.92%), Space: 19.9 MB (12.25%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -4,14 +4,14 @@ class Solution:
         a = [0] * len(s)
         
         b_count = 0
+        a_count = 0
         for i in range(len(s)):
             b[i] = b_count
             if s[i] == "b":
                 b_count += 1
-        a_count = 0
-        for i in range(len(s)-1,-1,-1):
-            a[i] = a_count
-            if s[i] == "a":
+                
+            a[-i-1] = a_count
+            if s[-i-1] == "a":
                 a_count += 1
         
         
