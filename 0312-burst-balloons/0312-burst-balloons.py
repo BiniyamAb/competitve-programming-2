79d4fe13092cfe08ea16d561c56236dad99a08e7
benj35 <se.biniyam.abiy@gmail.com>
2023-02-23 21:05:25 +0300

    Time: 5446 ms (85.33%), Space: 16.8 MB (89.36%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        coins = [1] + [i for i in nums if i > 0] + [1]
+        n = len(coins)
+        max_coins = [[0] * n for _ in range(n)]
+
+        for k in range(2, n):
+            for left in range(n - k):
+                right = left + k
+                for i in range(left + 1, right):
+                    max_coins[left][right] = \
+                        max(max_coins[left][right],
+                            coins[left] * coins[i] * coins[right] + 
+                            max_coins[left][i] +
+                            max_coins[i][right])
+
+        return max_coins[0][-1]
+        
